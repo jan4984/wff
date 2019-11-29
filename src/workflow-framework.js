fe41776f949a441b33e1354a80c89e29420d2793
shawn246 <jxu246@gmail.com>
2019-11-29 21:09:47 +0800

    fix bug

--- src/workflow-framework.js
+++ src/workflow-framework.js
@@ -218,7 +218,7 @@ class WorkflowFramework {
                     console.log('fail to addOperation:', e);
                     result = false;
                 });
-            if (result) {
+            if (!result) {
                 return complete.failure();
             }
         }
