b55b2126d0e8d5945baea1ecb96564041657ec07
YDJiang <jan4984@gmail.com>
2019-12-23 20:53:44 +0800

    remove unused module

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const WorkflowFramework = require('./src/workflow-framework');
-const DBService = require('./src/db-service');
-const FileService = require('./src/file-service');
 const {Logger, ActsWithRollback} = require('./src/utils');
 
 module.exports = {
-    WorkflowFramework, DBService, FileService, Logger, ActsWithRollback
+    WorkflowFramework, Logger, ActsWithRollback
 };
